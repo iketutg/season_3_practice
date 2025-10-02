f8dfd5d4f6a6513bb815b62a3df91a31d1eee002
I Ketut Gunawan <iketut.gunawan@vida.id>
2025-10-02 13:06:46 +0700

    menghapus file tidak perlu

--- bin/season_3_practice.dart
+++ bin/season_3_practice.dart
@@ -1,5 +1,4 @@
-import 'package:season_3_practice/season_3_practice.dart' as season_3_practice;
 
-void main(List<String> arguments) {
-  print('Hello world: ${season_3_practice.calculate()}!');
+void main() {
+  print('Hello world !!!');
 }
